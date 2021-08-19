2ff5efcee46599f231e002cd32eb0df6d4c33a4e
Oliver Booth <me@olivr.me>
2021-08-19 12:05:35 +0100

    Use static files built from front-end project

---
+++ front-end/scripts/copy2csproj.js
@@ -0,0 +1,6 @@
+﻿const fs = require("fs-extra");
+
+fs.copy("dist", "../UnityDocs/wwwroot", { overwrite: true }, (err => {
+    if (err) return console.error(err);
+    console.log("Copied successfully");
+}));
