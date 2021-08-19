10d2e65425fe961f58ccb5f654139e6c4d60e472
Oliver Booth <me@olivr.me>
2021-08-19 13:25:44 +0100

    Rewrite Program.cs as top-level program

--- UnityDocs/Program.cs
+++ UnityDocs/Program.cs
@@ -1,23 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using UnityDocs;
 
-namespace UnityDocs
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-    }
-}
+await Host.CreateDefaultBuilder(args)
+          .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+          .Build()
+          .RunAsync();
